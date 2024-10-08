dcc20d05f694524f5746542f3b1498540ef1377a
Atanas Dambov <92813305+adambov@users.noreply.github.com>
2024-10-08 22:47:49 +0300

    Bulgaria

--- TestClass.java
+++ TestClass.java
@@ -3,5 +3,7 @@ package SK16_Automation;
 public class TestClass {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Hello Bulgaria");
     }
 }
+
